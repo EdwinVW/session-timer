c433317680f44d06dedad1a4cb1119ff64e5bf39
EdwinVW <edwinvw@gmail.com>
2021-08-17 08:23:21 +0200

    Initial commit

---
+++ src/SessionTimer/Models/Schedule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SessionTimer.Models
+{
+    public class Schedule
+    {
+        private int _activeSectionIndex;
+
+        public string Name { get; private set; }
+
+        public List<Section> Sections { get; private set; }
+
+        public Schedule(string name)
+        {
+            Name = name;
+            Sections = new List<Section>();
+            Sections.ForEach(t => t.CurrentTime = t.Time);
+            _activeSectionIndex = 0;
+        }
+
+        public void AddSection(string name, string speaker, TimeSpan time)
+        {
+            bool enabled = Sections.Count == 0;
+            Sections.Add(new Section
+            {
+                Name = name,
+                Speaker = speaker,
+                Time = time,
+                CurrentTime = time,
+                Enabled = enabled
+            });
+        }
+
+        public void Tick()
+        {
+            var activeSection = Sections[_activeSectionIndex];
+            if (activeSection != null && activeSection.Enabled)
+            {
+                activeSection.CurrentTime = activeSection.CurrentTime.Subtract(TimeSpan.FromSeconds(1));
+                if (activeSection.CurrentTime == TimeSpan.Zero)
+                {
+                    activeSection.Enabled = false;
+                    if (_activeSectionIndex < Sections.Count - 1)
+                    {
+                        _activeSectionIndex++;
+                        Sections[_activeSectionIndex].Enabled = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/SessionTimer/Models/Section.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SessionTimer.Models
+{
+    public class Section
+    {
+        public string Name { get; set; }
+        public string Speaker { get; set; }
+        public TimeSpan Time { get; set; }
+        public TimeSpan CurrentTime { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
